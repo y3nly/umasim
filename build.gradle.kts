366cda34400f7fd79cd6c0da9a4d2642cc8d5ee3
y3nly <yen@y3n.net>
2026-02-17 14:43:54 -0800

    fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    application
 }
 
 group = "io.github.mee1080.umasim"
@@ -20,17 +19,15 @@ sourceSets {
 }
 
 dependencies {
-    // Standard Kotlin JSON parser
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-}
-
-application {
-    // Points to io/github/mee1080/umasim/race/Main.kt
-    mainClass.set("io.github.mee1080.umasim.race.MainKt")
 }
 
 // Configures the Shadow plugin to build the fat JAR
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    manifest {
+        attributes("Main-Class" to "io.github.mee1080.umasim.race.MainKt")
+    }
+    
     archiveBaseName.set("umasim-cli")
     archiveClassifier.set("all")
     archiveVersion.set("") 
